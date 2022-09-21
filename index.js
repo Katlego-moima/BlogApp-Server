844dda9c66a65355b12f92b968049e6f87ec5a28
katlego <moima@thedigitalacademy.co.za>
2022-09-21 14:28:02 +0200

    adding auth routes and testing them

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
-app.use("/", (req, res) => {
-  console.log("main");
-});
+dotenv.config();
+app.use(express.json()); //to use json objects
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+//routes
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running");
